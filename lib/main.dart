9d07df8ab0988d7383443a341a18ba4f68918b07
ueno12345 <ueno2022@s.okayama-u.ac.jp>
2024-08-01 23:02:17 +0900

    Import firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 import './homescreen.dart';
 import './orderscreen.dart';
 import './shopscreen.dart';
 import './inboxscreen.dart';
 
-void main() {
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
