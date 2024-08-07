ea844758d59f3a0dcc8546babb1a17f48b7b5d55
hirata <hirata2024@s.okayama-u.ac.jp>
2024-08-07 17:41:52 +0900

    Improve layout

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -9,6 +9,8 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final List<String> entries = <String>['1', '2', '3', '4', '5', '6', '7'];
+  final List<String> titles = <String>['JAVANICAN', "What's new", 'MOBILE ORDER', "What's new", '5', '6', '7'];
+  final List<String> texts = <String>['ようこそ', 'Daily\nOffering', 'レジに並ばず\nお店で受け取り', '4', '5', '6', '7'];
 
   @override
   Widget build(BuildContext context) {
@@ -19,8 +21,56 @@ class _HomeScreenState extends State<HomeScreen> {
             itemCount: entries.length,
             itemBuilder: (BuildContext context, int index) {
               return Container(
-              height: 200,
-              child: Center(child: Image.asset("images/image${entries[index]}.jpg")),
+                  height: 200,
+                  child: Stack(
+
+                    fit: StackFit.expand,
+                    children: [
+                      Container(
+                          child: Image.asset(
+                            "images/image${entries[index]}.jpg",
+                            fit: BoxFit.cover,
+                          ),
+                      ),
+                      Container(
+                        child: Column(
+                            children: <Widget>[
+                              Container(
+                                height: 50,
+                                child: Text(
+                                  titles[index],
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      color: Colors.white
+                                  ),
+                                ),
+                                alignment: Alignment.bottomLeft,
+                              ),
+                              Container(
+                                height: 100,
+                                child: Text(
+                                  texts[index],
+                                  style: TextStyle(
+                                      fontSize: 30,
+                                      color: Colors.white
+                                  ),
+                                ),
+                                alignment: Alignment.centerLeft,
+                              ),
+                              Container(
+                                height: 50,
+                                child: Icon(Icons.add_circle_outline,
+                                color: Colors.white),
+                                alignment: Alignment.centerLeft,
+                              )
+                        ]
+                        ),
+
+                      ),
+
+                    ],
+                  )
+
             );
             },
             separatorBuilder: (BuildContext context, int index) =>
